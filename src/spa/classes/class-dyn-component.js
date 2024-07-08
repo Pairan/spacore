d8fe47f0398cb7109032696b31a417a78668216e
Günter Hansen <guenter.hansen@it-p.de>
2024-07-08 07:38:19 +0200

    default css Class to "" not null

--- src/spa/classes/class-dyn-component.js
+++ src/spa/classes/class-dyn-component.js
@@ -9,7 +9,7 @@ export class DynComponentWithNav extends DynComponent {
     constructor(settings) {
         super(settings);
         this.text = settings.text || this.name;
-        this.icon = settings.icon || null;
+        this.icon = settings.icon || "";
         this.ual = settings.ual || null;
         this.group = settings.group || null;
         this.hideIt = settings.hideIt || false;
